2ec8af89e29e8a546eb7de977bd4804363feb175
rahul-kumar-09 <rahul7827759@gmail.com>
2025-04-06 18:11:01 +0530

    add link on logo

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -6,9 +6,9 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="flex flex-col bg-slate-100">
-      <div className="px-2 sm:pl-14 py-3 border-black border">
+      <Link href='/' className="px-2 sm:pl-14 py-3 border-black border">
         <Image src={assets.logo} width={120} alt="" />
-      </div>
+      </Link>
       <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
         <div className="w-[50%] sm:w-[80%] absolute right-0">
           <Link href='/admin/addProduct' className="flex items-center border border-black gap-3 font-medium px-3 py-3 bg-white shadow-[-5px_5px_0px_#000000]">
